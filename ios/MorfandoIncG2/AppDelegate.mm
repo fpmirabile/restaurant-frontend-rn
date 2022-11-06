b28a9026ceaa1e92ea5c8a18acc50f7b8b2adf73
Fernando Mirabile <fernando.mirabile@split.io>
2022-11-06 04:01:45 -0300

    Test

--- ios/MorfandoIncG2/AppDelegate.mm
+++ ios/MorfandoIncG2/AppDelegate.mm
@@ -44,7 +44,7 @@ static NSString *const kRNConcurrentRoot = @"concurrentRoot";
 #endif
 
   NSDictionary *initProps = [self prepareInitialProps];
-  UIView *rootView = RCTAppSetupDefaultRootView(bridge, @"AwesomeTSProject", initProps);
+  UIView *rootView = RCTAppSetupDefaultRootView(bridge, @"MorfandoIncG2", initProps);
 
   if (@available(iOS 13.0, *)) {
     rootView.backgroundColor = [UIColor systemBackgroundColor];
