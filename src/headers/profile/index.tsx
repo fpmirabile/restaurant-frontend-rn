835793eca8af1a9902d8c033cb0060393e066eab
Fernando Mirabile <fernando.mirabile@split.io>
2022-11-29 16:17:13 -0300

    Fixing sharing

--- src/headers/profile/index.tsx
+++ src/headers/profile/index.tsx
@@ -11,45 +11,47 @@ interface PropTypes {
   onShareClick?: () => void;
 }
 
-export function ProfileNavHeader({
-  onPressBack,
-  shareIcon,
-}: PropTypes) {
+export function ProfileNavHeader({ onPressBack, shareIcon }: PropTypes) {
   const { leftChevron: LeftChevron } = ICONS;
   const {
     view: { selectedRestaurant },
   } = useAppSelector(state => state.restaurant);
 
-  const name = selectedRestaurant?.name
-  const addres = selectedRestaurant?.address
-  const style = selectedRestaurant?.categories
-  const priceRange = selectedRestaurant?.priceRange
-  const stars = selectedRestaurant?.stars
-
   const onShare = async () => {
-      if(selectedRestaurant!= undefined)
-      {
-        let text = selectedRestaurant?.toString()
-        
-          try {
-            const result = await Share.share({
-              message:'Te recomiendo este restaurante. Se llama: ' +name+ ' su direccion es: '+addres +' su estilo de comida: '+style+ 'su rango de precio es: '+priceRange +' y su calificación es de: '+ stars +' estrellas',
-            });
-            if (result.action === Share.sharedAction) {
-              if (result.activityType) {
-                // shared with activity type of result.activityType
-              } else {
-                // shared
-              }
-            } else if (result.action === Share.dismissedAction) {
-              // dismissed
-            }
-          } catch (error) {
+    if (selectedRestaurant) {
+      const name = selectedRestaurant?.name;
+      const address = selectedRestaurant?.address;
+      const foodType = selectedRestaurant?.foodType;
+      const priceRange = selectedRestaurant?.priceRange;
+      const stars = selectedRestaurant?.stars;
 
+      try {
+        const result = await Share.share({
+          message:
+            'Te recomiendo este restaurante. Se llama: ' +
+            name +
+            ' su direccion es: ' +
+            address +
+            ' su estilo de comida: ' +
+            foodType +
+            'su rango de precio es: ' +
+            priceRange +
+            ' y su calificación es de: ' +
+            stars +
+            ' estrellas',
+        });
+        if (result.action === Share.sharedAction) {
+          if (result.activityType) {
+            // shared with activity type of result.activityType
+          } else {
+            // shared
           }
-
-      
-  }}
+        } else if (result.action === Share.dismissedAction) {
+          // dismissed
+        }
+      } catch (error) {}
+    }
+  };
 
   return (
     <View style={styles.headerContainer}>
@@ -58,7 +60,7 @@ export function ProfileNavHeader({
         <TouchableText message="Volver" onPress={onPressBack} type="ctaText" />
       </View>
       <View style={styles.socialShareContainer}>
-        {shareIcon == true ? (
+        {shareIcon ? (
           <ImageButton imageSvg={ICONS.socialShare} onPress={onShare} />
         ) : (
           <View />
